7e2fe804f16b58f62cb73fb58c25645757d7512f
islam7x <92950976+islam7x@users.noreply.github.com>
2021-12-11 03:34:39 +0300

    Added categories views & controllers (#6)
    * add crud
    
    * add crud
    
    * add crud
    
    * add crud
    
    * add crud
    
    * error correction
    
    * error correction 2
    
    Co-authored-by: kkk_is7 <kkk_is7@MacBook-Air-kkk-is7.local>

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit destroy update]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    redirect_to categories_path unless @category
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
