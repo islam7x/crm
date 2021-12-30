65dd92c2172ca1164c3b790145914895f8b5e017
islam7x <92950976+islam7x@users.noreply.github.com>
2021-12-30 18:03:33 +0300

    add roles to user (#14)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: { standard: 0, admin: 1 }
 end
