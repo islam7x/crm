659b8dc115b79df0243a9f6464897aea1c30c356
kkk_is7 <kkk_is7@MacBook-Air-kkk-is7.local>
2022-01-01 20:41:00 +0300

    fix errors

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
-class Admin::BaseController < ApplicationController
+module Admin
+  class BaseController < ApplicationController
+  end
 end
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,56 +1,58 @@
 # frozen_string_literal: true
 
-class Admin::CategoriesController < Admin::BaseController
-  before_action :set_category, only: %i[show edit destroy update]
-  before_action :set_columns, only: %i[show]
+module Admin
+  class CategoriesController < BaseController
+    before_action :set_category, only: %i[show edit destroy update]
+    before_action :set_columns, only: %i[show]
 
-  def index
-    @categories = Category.all
-  end
-
-  def new
-    @category = Category.new
-  end
-
-  def edit; end
-
-  def update
-    if @category.update(category_params)
-      redirect_to admin_category_path(@category)
-    else
-      render 'edit'
+    def index
+      @categories = Category.all
     end
-  end
 
-  def show
-    redirect_to admin_categories_path unless @category
-  end
-
-  def create
-    @category = Category.new(category_params)
-    if @category.save
-      redirect_to admin_category_path(@category)
-    else
-      render 'new'
+    def new
+      @category = Category.new
     end
-  end
 
-  def destroy
-    @category.destroy
-    redirect_to admin_categories_path
-  end
+    def edit; end
 
-  private
+    def update
+      if @category.update(category_params)
+        redirect_to admin_category_path(@category)
+      else
+        render 'edit'
+      end
+    end
 
-  def set_columns
-    @columns = @category.columns
-  end
+    def show
+      redirect_to admin_categories_path unless @category
+    end
 
-  def set_category
-    @category = Category.find(params[:id])
-  end
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        redirect_to admin_category_path(@category)
+      else
+        render 'new'
+      end
+    end
 
-  def category_params
-    params.require(:category).permit(:name)
+    def destroy
+      @category.destroy
+      redirect_to admin_categories_path
+    end
+
+    private
+
+    def set_columns
+      @columns = @category.columns
+    end
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
   end
 end
