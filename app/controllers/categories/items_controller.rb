4f5d40eb615e889b5a22400dcfd057bfab6698c1
kkk_is7 <kkk_is7@MacBook-Air-kkk-is7.local>
2022-01-07 22:57:49 +0300

    fix errors

---
+++ app/controllers/categories/items_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module Categories
+  class ItemsController < ApplicationController
+    before_action :set_item, only: %i[edit destroy update]
+    before_action :set_category, only: %i[update edit destroy create new]
+    before_action :set_columns, only: %i[new edit]
+
+    def index
+      @items = Item.all
+    end
+
+    def new
+      @item = Item.new
+    end
+
+    def create
+      @item = @category.items.new(item_params)
+      if @item.save
+        redirect_to category_items_path(@category)
+      else
+        render 'new'
+      end
+    end
+
+    def edit; end
+
+    def update
+      if @item.update(item_params)
+        redirect_to category_items_path(@category)
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @item.destroy
+      redirect_to category_items_path(@category)
+    end
+
+    private
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def set_columns
+      @columns = @category.columns
+    end
+
+    def item_params
+      params.require(:item).permit(:quantity, :weight, :column_id, :datetime_of_create)
+    end
+
+    def set_category
+      @category = Category.find(params[:category_id])
+    end
+  end
+end
