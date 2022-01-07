a76e85367fb9cc8fd3f0d59058a61c5c7a2cf049
kkk_is7 <kkk_is7@MacBook-Air-kkk-is7.local>
2022-01-07 23:11:52 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
   resources :categories do
     resources :items, expect: %i[show]
   end
+
   namespace :admin do
     resources :categories do
       resources :columns, except: %i[index show], module: :categories
     end
   end
+
   root 'admin/categories#index'
 end
