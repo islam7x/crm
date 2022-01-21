7c397c96345d7399b70b009425dcf433f7024f9e
kkk_is7 <kkk_is7@MacBook-Air-kkk-is7.local>
2022-01-21 15:10:07 +0300

    add filter

--- app/services/categories/items/list_service.rb
+++ app/services/categories/items/list_service.rb
@@ -3,10 +3,16 @@
 module Categories
   module Items
     class ListService < ::ApplicationService
-      attr_accessor :category
+      attr_accessor :category, :from, :to
 
       def process
-        @result = category.items.order('items.date_of_create DESC, columns.position ASC').group_by(&:date_of_create)
+        @result = category
+          .items
+          .where('date_of_create >= :from AND date_of_create <= :to',
+                 from: from.presence || Date.current.beginning_of_month,
+                 to: to.presence || Date.current.end_of_month)
+          .order('items.date_of_create DESC, columns.position ASC')
+          .group_by(&:date_of_create)
       end
     end
   end
