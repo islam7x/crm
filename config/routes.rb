9d941b926cfdfac152bfd7f2445ffbaeb66bf231
islam7x <92950976+islam7x@users.noreply.github.com>
2022-02-10 22:29:55 +0300

    baning registration (#25)
    * baning registration
    
    * baning registration 1
    
    * password recovery disabled
    
    * delete views
    
    Co-authored-by: kkk_is7 <kkk_is7@MacBook-Air-kkk-is7.local>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: %i[registrations password]
 
   resources :categories do
     resources :items, expect: %i[show], module: :categories
