ac746b483df3022b12b9346832d12288997f792c
kkk_is7 <kkk_is7@MacBook-Air-kkk-is7.local>
2022-02-10 15:49:16 +0300

    baning registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, skip: :registrations
 
   resources :categories do
     resources :items, expect: %i[show], module: :categories
