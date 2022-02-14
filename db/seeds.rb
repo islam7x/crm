75434c2de699a0af3a3b9e472a964d20e21b961b
kkk_is7 <kkk_is7@MacBook-Air-kkk-is7.local>
2022-02-14 20:00:08 +0300

    added script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+columns = Category.find(45).columns
+
+500.times do |i|
+  columns.each do |column|
+    item = Item.find_or_initialize_by(column_id: column.id, category_id: column.category_id, date_of_create: i.days.ago)
+    item.assign_attributes(
+      quantity: Random.rand(100),
+      weight: Random.rand(500.0).round(2)
+    )
+
+    item.save!
+  end
+end
