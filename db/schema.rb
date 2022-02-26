c6a89cb8f030a675444fdabd73a8c9db4bd7b0d6
islam7x <92950976+islam7x@users.noreply.github.com>
2022-02-26 15:06:03 +0300

    added model expense (#29)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_050641) do
+ActiveRecord::Schema.define(version: 2022_02_26_072321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 2022_02_26_050641) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position", default: 1, null: false
     t.index ["category_id"], name: "index_columns_on_category_id"
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "parish_id"
+    t.integer "killed"
+    t.integer "killed_weight"
+    t.integer "sold_count"
+    t.float "sold_weight"
+    t.date "date_of_create"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["parish_id"], name: "index_expenses_on_parish_id"
   end
 
   create_table "items", force: :cascade do |t|
